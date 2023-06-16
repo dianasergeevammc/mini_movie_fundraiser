393bf17c4ca244c480d2de13c66753d7c13356d7
euuedeus <136386900+euuedeus@users.noreply.github.com>
2023-06-16 14:26:31 +1200

    Add files via upload

---
+++ yes_no_v1.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# main routine
+want_instructions = input("do you want to read the instructions? ")
+
+if want_instructions == "yes":
+    print("nuh uh")
+elif want_instructions == "no":
+    pass
+else:
+    print("you gotta answer yes/no")
+    
