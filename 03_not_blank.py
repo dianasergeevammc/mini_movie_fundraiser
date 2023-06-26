f7c0400cf37bf4648feded7c8179a3e6f2332ea1
euuedeus <136386900+euuedeus@users.noreply.github.com>
2023-06-27 09:28:40 +1200

    Add files via upload

---
+++ 03_not_blank.py
@@ -0,0 +1,22 @@
+# function goes here
+
+# checks if response isnt blank
+def not_blank(question):
+
+    while True:
+        response = input(question)
+        if response == "":
+            print("Sorry this cant be blank. Try again.. ")
+
+        else:
+            return response
+
+# main routine goes here
+
+
+while True:
+    name = not_blank("Enter your name(or 'xxx' to quit) ")
+    if name == "xxx":
+        break
+
+print ("we are done.")
